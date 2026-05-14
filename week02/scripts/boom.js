09ffd464dce1e6f7af0df277ec520c24b1ee3fd3
nansukusaketrine3-cell <nansukusaketrine3@gmail.com>
2026-05-14 16:28:56 +0300

    commit

--- week02/scripts/boom.js
+++ week02/scripts/boom.js
@@ -1,9 +1,26 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('text');
-const li = document.createElement('li');
-const deleteButton  = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = 'X';
-li.append(deleteButton);
-list.append(li);
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+
+    if (input.value === '') {
+        return;
+    }
+
+    const li = document.createElement('li');
+    li.textContent = input.value;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '❌';
+
+    deleteButton.addEventListener('click', function () {
+        list.removeChild(li);
+    });
+
+    li.appendChild(deleteButton);
+    list.appendChild(li);
+
+    input.value = '';
+    input.focus();
+});
