fe8d40e324be24478f48a7e06dfa205bee2cb73d
nansukusaketrine3-cell <nansukusaketrine3@gmail.com>
2026-06-02 09:26:11 +0300

    commit

---
+++ week04/scripts/lazyloading.js
@@ -0,0 +1,7 @@
+// Current year
+document.getElementById("currentyear").textContent =
+    new Date().getFullYear();
+
+// Last modified date
+document.getElementById("lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
